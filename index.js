dc968c3f351c3a264bf34b0586649691fee43578
17aa9b2acda8581d5915f4cca43027d2ea98b089 <geethaselvaraj@optisolbusiness.com>
2021-07-29 11:41:01 +0530

    Signin token with name

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ async function signJWT(userid, tenantId) {
         let JWT_TOKEN_SECRET = await generateSecretValue();
         let iat = Date.now();
         let exp = Math.round((iat / 1000) + 24 * 60 * 60);
-        return jwt.sign({ userid, tenantId, iat, exp }, JWT_TOKEN_SECRET)
+        return jwt.sign({ name: userid, userid, tenantId, iat, exp }, JWT_TOKEN_SECRET)
     } else {
         return null;
     }
